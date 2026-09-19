4d39da883f645dd3e27447abea8ab56f82f6587a
nadyaasekar <nadya.sekar@ui.ac.id>
2026-09-19 21:02:52 +0700

    fix: add pws domain to CSRF_TRUSTED_ORIGINS

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -152,6 +152,6 @@ WHITENOISE_USE_FINDERS = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Implementasi Form CSRF
-CSRF_TRUSTED_ORIGINS = ["https://nadya-sekar-myportofolio.pws.cs.ui.ac.id/",
+CSRF_TRUSTED_ORIGINS = ["https://nadya-sekar-myportofolio.pws.cs.ui.ac.id",
                         "http://localhost:8000",
                         "http://127.0.0.1:8000",]
